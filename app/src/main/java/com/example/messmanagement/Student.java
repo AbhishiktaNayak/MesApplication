46c687173f3c705caa19bba14ad6eaac50ce5301
Abhishikta24 <abhishiktanayak@gmail.com>
2020-03-26 21:27:23 +0530

    User Authentication and NAvigation View

---
+++ app/src/main/java/com/example/messmanagement/Student.java
@@ -0,0 +1,18 @@
+package com.example.messmanagement;
+
+public class Student {
+    public String sname,sroll,semail,sbranch,shostel,sphone;
+
+    public Student(String name, String roll, String email, String pw, String branch, String hostel, String phone){
+
+    }
+
+    public Student(String sname, String sroll, String semail, String sbranch, String shostel, String sphone) {
+        this.sname = sname;
+        this.semail = semail;
+        this.sroll = sroll;
+        this.sbranch = sbranch;
+        this.shostel = shostel;
+        this.sphone = sphone;
+    }
+}
